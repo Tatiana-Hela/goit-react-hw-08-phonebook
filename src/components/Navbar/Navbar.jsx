e329cba2d01113fb91afa04c25b6bd8a18a30e1c
Tatiana-Hela <malya9472@gmail.com>
2023-03-16 15:35:46 +0300

    add //

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,3 +36,40 @@ const Navbar = () => {
   );
 };
 export default Navbar;
+
+// @import url("https://fonts.googleapis.com/css2?family=Material+Icons");
+
+// .material-icons {
+//   font-family: "Material Icons";
+//   font-weight: normal;
+//   font-style: normal;
+//   font-size: 24px;
+//   line-height: 1;
+//   letter-spacing: normal;
+//   text-transform: none;
+//   display: inline-block;
+//   white-space: nowrap;
+//   word-wrap: normal;
+//   direction: ltr;
+//   -webkit-font-feature-settings: "liga";
+//   -webkit-font-smoothing: antialiased;
+// }
+
+// import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+
+// function ThemeSwitcher() {
+//   const { colorMode, toggleColorMode } = useColorMode();
+//   const isChecked = colorMode === 'dark';
+
+//   return (
+//     <Switch
+//       marginLeft="20px"
+//       isChecked={isChecked}
+//       onChange={toggleColorMode}
+//       icons={{
+//         checked: <MoonIcon as={MaterialIcon} className="material-icons" />,
+//         unchecked: <SunIcon as={MaterialIcon} className="material-icons" />,
+//       }}
+//     ></Switch>
+//   );
+// }
