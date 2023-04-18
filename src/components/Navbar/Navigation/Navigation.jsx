96a094679897f9c4f43392d38af3cad009a0bff4
Tatiana-Hela <malya9472@gmail.com>
2023-04-18 13:20:40 +0300

    response

--- src/components/Navbar/Navigation/Navigation.jsx
+++ src/components/Navbar/Navigation/Navigation.jsx
@@ -1,9 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks/useAuth';
+import { IconButton } from '@chakra-ui/react';
+import { Icon } from '@chakra-ui/icons';
+import { RiContactsFill } from 'react-icons/ri';
 import css from '../Navigation/Navigation.module.css';
 
 const Navigation = () => {
   const { isLoggedIn } = useAuth();
+
+  const isMobile = window.innerWidth < 768;
 
   return (
     <nav>
@@ -11,7 +16,16 @@ const Navigation = () => {
         <NavLink to="/" className={css.linkPB}>
           Phonebook
         </NavLink>
-        {isLoggedIn && (
+        {isLoggedIn && isMobile ? (
+          <IconButton backgroundColor="#fff" ml="6px">
+            <Icon
+              as={RiContactsFill}
+              width="24px"
+              height="24px"
+              color="#589F2D"
+            />
+          </IconButton>
+        ) : (
           <NavLink to="/contacts" className={css.link}>
             Contacts
           </NavLink>
@@ -20,4 +34,5 @@ const Navigation = () => {
     </nav>
   );
 };
+
 export default Navigation;
