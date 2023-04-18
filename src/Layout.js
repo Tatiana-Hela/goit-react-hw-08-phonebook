6edd4ff8b9f04ffd4b1a54998a29bd53afd97ee4
Tatiana-Hela <malya9472@gmail.com>
2023-04-18 13:55:08 +0300

    src

--- src/Layout.js
+++ src/Layout.js
@@ -2,7 +2,7 @@ import Navbar from 'components/Navbar/Navbar';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
-import Footer from 'components/Footer/Footer';
+import Footer from '../src/components/Footer/Footer';
 
 const Layout = () => {
   return (
